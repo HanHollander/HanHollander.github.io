0d608d48046e0d08ebcc2a872be1bb3ae8bd52a5
HanHollander <han_hollander@hotmail.nl>
2017-11-05 17:58:03 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -2,7 +2,7 @@ function open_menu() {
     var x = document.getElementById("menu");
     x.style.width = "30%";
     x.style.fontSize = "16px";
-    x.style.paddingTop = "10%";
+    // x.style.paddingTop = "10%";
     x.style.display = "block";
 }
 function close_menu() {
