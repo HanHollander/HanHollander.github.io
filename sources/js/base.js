a322bdec471bd8348382747cbb477ad41ab2ab40
HanHollander <han_hollander@hotmail.nl>
2017-11-16 15:25:30 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,6 +1,6 @@
 function open_menu() {
     var x = document.getElementById("menu");
-    x.style.width = "250px";
+    x.style.width = "10%";
     x.style.fontSize = "16px";
     // x.style.paddingTop = "10%";
     x.style.display = "block";
