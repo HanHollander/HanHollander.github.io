49db67a30085117931409691f97184d42f7c3678
HanHollander <han_hollander@hotmail.nl>
2017-11-16 15:54:11 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -8,3 +8,14 @@ function open_menu() {
 function close_menu() {
     document.getElementById("menu").style.display = "none";
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
