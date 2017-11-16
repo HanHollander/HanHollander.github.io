2202820933ae456730e45ad9e2dcb4af60f6e8d3
HanHollander <han_hollander@hotmail.nl>
2017-11-16 15:57:57 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,11 +1,9 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("page").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById("page").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
 }
