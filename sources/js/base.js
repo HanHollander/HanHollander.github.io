003f7ba49a05266b1bf3cc513deb2e8b3024ef23
HanHollander <han_hollander@hotmail.nl>
2017-11-16 15:55:44 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,21 +1,11 @@
-function open_menu() {
-    var x = document.getElementById("menu");
-    x.style.width = "12%";
-    x.style.fontSize = "16px";
-    // x.style.paddingTop = "10%";
-    x.style.display = "block";
-}
-function close_menu() {
-    document.getElementById("menu").style.display = "none";
-}
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("page").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("page").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
 }
