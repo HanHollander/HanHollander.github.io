fa211ac469e32536a0d8a48816bf347ae60012fd
HanHollander <han_hollander@hotmail.nl>
2017-11-17 21:28:03 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,14 +1,14 @@
 function topbar() {
-    var x = document.getElementById("topbar")
-    var y = document.getElementById("topbar-dd");
-    var z = document.getElementById("topbar-dd-i");
-    if (y.style.display == "none") {
+    var x = document.getElementById("topbar-dd");
+    var y = document.getElementById("topbar-dd-i");
+    var z = document.getElementById("topbar-ddc")
+    if (z.style.display == "none") {
         x.style.display = "block"
-        y.style.display = "block";
+        z.style.display = "block";
         // z.style.transform = "rotate(90)"
     } else {
         x.style.display = "inline-block"
-        y.style.display = "none";
+        z.style.display = "none";
     }
 
 }
