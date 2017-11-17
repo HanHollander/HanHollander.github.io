85e20b24478632620f7dbe16142857623fcf3dbb
HanHollander <han_hollander@hotmail.nl>
2017-11-17 22:31:12 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -14,13 +14,35 @@ function topbar() {
     var x = document.getElementById("topbar-dd");
     var y = document.getElementById("topbar-dd-i");
     var z = document.getElementById("topbar-ddc")
-    if (z.style.display == "block") {
+    if (z.style.transform == scaleY(1)) {
         x.style.display = "inline-block"
-        z.style.display = "none";
+        var sy = 1;
+        var dsy = 0.01;
+        var timer;
+        timer = setInterval(
+            function() {
+                sy = sy - dsy;
+                z.style.transform = scaleY(sy);
+                // clear the timer at 400px to stop the animation
+                if (sy == 0) {
+                    clearInterval(timer);
+                }
+            }, 10);
         y.style.transform = "rotate(0deg)"
     } else {
         x.style.display = "block"
-        z.style.display = "block";
+        var sy = 0;
+        var dsy = 0.01;
+        var timer;
+        timer = setInterval(
+            function() {
+                sy = sy + dsy;
+                z.style.transform = scaleY(sy);
+                // clear the timer at 400px to stop the animation
+                if (sy == 1) {
+                    clearInterval(timer);
+                }
+            }, 10);
         y.style.transform = "rotate(180deg)"
     }
 }
