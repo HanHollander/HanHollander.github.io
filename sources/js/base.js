1833daf8541816fc404c112f2b132f08ff4d29c8
HanHollander <han_hollander@hotmail.nl>
2017-11-17 21:20:32 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,10 +1,13 @@
 function topbar() {
+    var x = document.getElementById("topbar")
     var y = document.getElementById("topbar-dd");
     var z = document.getElementById("topbar-dd-i");
     if (y.style.display == "none") {
+        x.style.display = "block"
         y.style.display = "block";
-        z.style.transform = "rotate(90)"
+        // z.style.transform = "rotate(90)"
     } else {
+        x.style.display = "inline-block"
         y.style.display = "none";
     }
 
