29a22a749ecf49db9018666ddfbb36b9e2ef692d
HanHollander <han_hollander@hotmail.nl>
2017-11-17 21:32:57 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,3 +1,15 @@
+function languagebar() {
+    var x = document.getElementById("languagebar-dd");
+    var z = document.getElementById("languagebar-ddc")
+    if (z.style.display == "none") {
+        x.style.display = "block"
+        z.style.display = "block";
+    } else {
+        x.style.display = "inline-block"
+        z.style.display = "none";
+    }
+}
+
 function topbar() {
     var x = document.getElementById("topbar-dd");
     var y = document.getElementById("topbar-dd-i");
@@ -5,10 +17,9 @@ function topbar() {
     if (z.style.display == "none") {
         x.style.display = "block"
         z.style.display = "block";
-        y.style.transform = "rotate(90)"
+        y.style.transform = "rotate(180deg)"
     } else {
         x.style.display = "inline-block"
         z.style.display = "none";
     }
-
 }
