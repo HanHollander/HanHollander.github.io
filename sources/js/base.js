2d48645cdf8439c156e3620a1c1d9cf55e032a16
HanHollander <han_hollander@hotmail.nl>
2017-11-17 23:15:51 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -1,12 +1,43 @@
+var languagebartimer;
 function languagebar() {
+    clearInterval(languagebartimer);
     var x = document.getElementById("languagebar-dd");
     var z = document.getElementById("languagebar-ddc")
-    if (z.style.display == "block") {
-        x.style.display = "inline-block"
-        z.style.display = "none";
+    var sy;
+    var dsy;
+    if (x.style.display == "block") {
+        x.style.display = "inline-block";
+        sy = 1;
+        z.style.transform = "scaleY(1)";
+        dsy = 0.1;
+        languagebartimer = setInterval(
+            function() {
+                sy = sy - dsy;
+                console.log(sy);
+                z.style.transform = "scaleY("+ sy + ")";
+                // clear the timer at 400px to stop the animation
+                if (sy < 0) {
+                    clearInterval(languagebartimer);
+                    z.style.transform = "scaleY(0)";
+                }
+            }, 25);
+        y.style.transform = "rotate(0deg)";
     } else {
         x.style.display = "block"
-        z.style.display = "block";
+        sy = 0;
+        z.style.transform = "scaleY(0)";
+        dsy = 0.1;
+        languagebartimer = setInterval(
+            function() {
+                sy = sy + dsy;
+                z.style.transform = "scaleY("+ sy + ")";
+                // clear the timer at 400px to stop the animation
+                if (sy > 1) {
+                    clearInterval(languagebartimer);
+                    z.style.transform = "scaleY(1)";
+                }
+            }, 25);
+        y.style.transform = "rotate(180deg)"
     }
 }
 
