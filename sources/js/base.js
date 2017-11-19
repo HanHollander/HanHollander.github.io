279850e21998a1025bea36d9ff32c7551e9c8bd4
HanHollander <han_hollander@hotmail.nl>
2017-11-19 23:24:26 +0100

    c

--- sources/js/base.js
+++ sources/js/base.js
@@ -54,37 +54,42 @@ function topbar() {
     var y = document.getElementById("topbar-dd-i");
     var z = document.getElementById("topbar-ddc");
     var sy;
+    var hy=60;
     var dsy;
     if (x.style.display == "block") {
         x.style.display = "inline-block";
         sy = 1;
-        z.style.transform = "scaleY(1)";
+        // z.style.transform = "scaleY(1)";
         dsy = 0.1;
         topbartimer = setInterval(
             function() {
                 sy = sy - dsy;
                 console.log(sy);
-                z.style.transform = "scaleY("+ sy + ")";
+                // z.style.transform = "scaleY("+ sy + ")";
+                z.style.height = (sy*hy) + "px";
                 // clear the timer at 400px to stop the animation
                 if (sy < 0) {
                     clearInterval(topbartimer);
-                    z.style.transform = "scaleY(0)";
+                    // z.style.transform = "scaleY(0)";
+                    z.style.height = "0px";
                 }
             }, 25);
         y.style.transform = "rotate(0deg)";
     } else {
         x.style.display = "block"
         sy = 0;
-        z.style.transform = "scaleY(0)";
+        // z.style.transform = "scaleY(0)";
         dsy = 0.1;
         topbartimer = setInterval(
             function() {
                 sy = sy + dsy;
-                z.style.transform = "scaleY("+ sy + ")";
+                // z.style.transform = "scaleY("+ sy + ")";
+                z.style.height = (sy*hy) + "px";
                 // clear the timer at 400px to stop the animation
                 if (sy > 1) {
                     clearInterval(topbartimer);
                     z.style.transform = "scaleY(1)";
+                    z.style.height = "60px";
                 }
             }, 25);
         y.style.transform = "rotate(180deg)"
