1df9bfb2bbc66b3e80b74880a208b404b8d82685
HanHollander <han_hollander@hotmail.nl>
2017-11-21 11:59:42 +0100

    j

--- sources/js/base.js
+++ sources/js/base.js
@@ -59,7 +59,7 @@ function topbar() {
     var h = document.getElementById("topbar-home");
     var hys = h.style.height;
     var pad = h.style.paddingTop;
-    console.log(x.style.height);
+    console.log(x.style.display);
     console.log(pad);
     if (x.style.display == "block") {
         x.style.display = "inline-block";
