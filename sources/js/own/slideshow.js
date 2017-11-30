422237fc68e04edb320ac2d487bfba1cc4e9b67a
HanHollander <han_hollander@hotmail.nl>
2017-11-30 17:47:05 +0100

    c

--- sources/js/own/slideshow.js
+++ sources/js/own/slideshow.js
@@ -2,7 +2,7 @@ bs[0].style.backgroundColor = "#d0d0d0"
 function slideshowbutton(curID) {
     var bs = document.getElementsByClassName("my-slider-button");
     var i;
-    for (i = 0; i < length(bs); i++) {
+    for (i = 0; i < bs.length; i++) {
         bs[i].style.backgroundColor = "#e0e0e0"
     }
     bs[curID].style.backgroundColor = "#d0d0d0"
