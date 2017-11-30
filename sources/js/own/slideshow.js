e42171c7d4c2cc8b64e0a0327dd92d7792fc63ed
HanHollander <han_hollander@hotmail.nl>
2017-11-30 21:54:03 +0100

    c

--- sources/js/own/slideshow.js
+++ sources/js/own/slideshow.js
@@ -1,9 +1,11 @@
 slideshowbutton(0)
+var sf = document.getElementById("slider-figure");
+sf.style.left="0%";
 function slideshowbutton(curID) {
     var bs = document.getElementsByClassName("my-slider-button");
     var i;
     for (i = 0; i < bs.length; i++) {
-        bs[i].style.backgroundColor = "#e0e0e0"
+        bs[i].style.backgroundColor = "#e0e0e0";
     }
-    bs[curID].style.backgroundColor = "#d0d0d0"
+    bs[curID].style.backgroundColor = "#d0d0d0";
 }
