4f10181ce33ecc2bad0bce7742dbd5176522373e
HanHollander <han_hollander@hotmail.nl>
2017-11-30 22:09:50 +0100

    c

--- sources/js/own/slideshow.js
+++ sources/js/own/slideshow.js
@@ -1,5 +1,15 @@
-slideshowbutton(0)
+var cs = 0;
+var slides = 3;
+slideshowauto();
 var sf = document.getElementById("slider-figure");
+
+function slideshowauto() {
+    slideshowbutton(cs);
+    slideshowpicture(cs);
+    cs = (cs) % slides;
+    setTimeout(slideshowauto(cs), 2000);
+}
+
 sf.style.left="0%";
 function slideshowbutton(curID) {
     var bs = document.getElementsByClassName("my-slider-button");
@@ -8,4 +18,22 @@ function slideshowbutton(curID) {
         bs[i].style.backgroundColor = "#e0e0e0";
     }
     bs[curID].style.backgroundColor = "#d0d0d0";
+    cs = curID
+}
+
+function slideshowpicture(curID) {
+    var fig = document.getElementsByClassName("slider-figure");
+    fig.style.left = (100 * curID) + "%";
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
